16f991e7d3b64d491732c571b4aa78b5988f7e01
Micky8a <brianezequiel8a@gmail.com>
2023-09-11 17:56:48 -0300

    nuevas correcciones

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ const $filtrado = document.getElementById ('checkId');
 let $search = document.getElementById('searchInput');
 
 function estructuraTarjeta (datos){
-    return `<div class="card col-md-4 stile-card" style="width: 18rem">
+    return `<div class="card col-md-4 stile-card" style="width: 18rem;">
     <img src="${datos.image}" class="card-img-top" alt="..." />
     <div class="card-body">
       <h5 class="card-title">${datos.name}</h5>
@@ -38,15 +38,13 @@ $filtrado.addEventListener( "change" , filtrosCruzados);
   //filtrado por check
    function filtroPorCheck(array){
      const nodeListCheck = document.querySelectorAll("input[type=checkbox]:checked");
-     console.log(nodeListCheck);
      const arrayChech = Array.from(nodeListCheck);
-     console.log(arrayChech);
      const arrayValores = arrayChech.map( input => input.value);
-     console.log(arrayValores);
      const filtradosCheck = array.filter(evento=>(arrayValores.includes(evento.category))|| arrayValores.length==0);
      return(filtradosCheck);
   }
 
+  //id boton
   let $button = document.getElementById('searchButton');
 
   //escuchador de search
@@ -55,7 +53,7 @@ $filtrado.addEventListener( "change" , filtrosCruzados);
     filtrosCruzados();
     $search.value = "";
   });
-
+// filtro search
   function filtroSearch(array, input){
     let tarjetaFiltrada = array.filter(tarjetas => tarjetas.name.toLowerCase().includes(input.toLowerCase()))
     return tarjetaFiltrada;
@@ -64,5 +62,5 @@ $filtrado.addEventListener( "change" , filtrosCruzados);
   //filtro cruzado
   function filtrosCruzados(){
     let filtroTotal = filtroSearch(filtroPorCheck(data.events), $search.value)
-    insertarTarjetas(listaTarjeta(filtroTotal), "tarjetasId");
+    insertarTarjetas(listaTarjeta(filtroTotal), "tarjetasId"); //
 };
